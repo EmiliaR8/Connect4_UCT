884af200e5e5bffcdff24013a123a492bc128cd8
EmiliaR8 <rivasemilia2@gmail.com>
2025-04-07 23:46:11 -0600

    Fixing drivers and UCT prime

--- driver_part3.py
+++ driver_part3.py
@@ -2,129 +2,102 @@ import numpy as np
 from Board import Board
 from strategy import UCT, UCT_prime
 from collections import defaultdict
-
+import time
 
 def test():
-    """
-    Play a single game of Connect Four between the baseline UCT and the improved UCT.
-    Allows the user to select algorithms for Red and Yellow players.
-    """
-    #Generate an empty board
+    #generate empty board 
     board = Board()
-
-    #Prompt user to select algorithms for Red and Yellow players
-    print("Select algorithm for Red player:")
-    print("1: Baseline UCT (10000 simulations)")
-    print("2: Improved UCT (10000 simulations)")
-    red_alg_choice = int(input("Enter choice (1 or 2): "))
-    if red_alg_choice == 1:
-        board.bindPlayer(UCT(simulations=10000), "R")
-    elif red_alg_choice == 2:
-        board.bindPlayer(UCT_prime(simulations=10000), "R")
+    
+    alg = int(input("Please the algorithm you want to use for player 1 RED (1:UCT, 2: UCT Prime): "))
+    if alg == 1:
+        num = int(input("Please the number of simulations for UCT: "))
+        board.bindPlayer(UCT(simulations = num), "R")
+    elif alg == 2:
+        num = int(input("Please the number of simulations for UCT prime: "))
+        board.bindPlayer(UCT_prime(simulations = num), "R")
     else:
-        print("Invalid choice for Red player.")
-        return
+        print("wrong")
 
-    print("Select algorithm for Yellow player:")
-    print("1: Baseline UCT (10000 simulations)")
-    print("2: Improved UCT (10000 simulations)")
-    yellow_alg_choice = int(input("Enter choice (1 or 2): "))
-    if yellow_alg_choice == 1:
-        board.bindPlayer(UCT(simulations=10000), "Y")
-    elif yellow_alg_choice == 2:
-        board.bindPlayer(UCT_prime(simulations=10000), "Y")
+    alg = int(input("Please the algorithm you want to use for player 2 Yelliw (1:UCT, 2: UCT Prime): "))
+    if alg == 1:
+        num = int(input("Please the number of simulations for UCT: "))
+        board.bindPlayer(UCT(simulations = num), "Y")
+    elif alg == 2:
+        num = int(input("Please the number of simulations for UCT prime: "))
+        board.bindPlayer(UCT_prime(simulations = num), "Y")
     else:
-        print("Invalid choice for Yellow player.")
-        return
+        print("wrong")
 
-    #Play the game
+    start_time = time.process_time()
     game_result = None
     while game_result is None:
-        game_result = board.turn(verbosity="None", parameter=10000)
+        game_result = board.turn(verbosity = "None", parameter = num)
 
-    #Print the result
     if game_result == -1:
         print("Red wins!")
     elif game_result == 1:
         print("Yellow wins!")
     else:
         print("Draw!")
-
-    #Print the final board state
+    end_time = time.process_time()
+    
+    print(end_time - start_time)
     print(board.board)
 
 
-def run_tournament():
-    """
-    Run a tournament comparing the baseline UCT (10000 simulations) with the improved UCT.
-    """
-    #Define the algorithms to test
+def run_tournament(alg_names_r, alg_names_y):
     alg_configs = {
-        "UCT-10000": lambda: UCT(simulations=10000), #Baseline UCT
-        "Improved-UCT-10000": lambda: UCT_prime(simulations=10000) #Improved UCT
+        "UCT-10000": lambda: UCT(simulations=1000), #Baseline UCT
+        "Improved-UCT-10000": lambda: UCT_prime(simulations=1000) #Improved UCT
     }
 
     results = defaultdict(lambda: defaultdict(int))
     alg_names = list(alg_configs.keys())
+    
     game = 1
-
-    for i in range(len(alg_names)):
-        for j in range(len(alg_names)):
-            alg1_name = alg_names[i]
-            alg2_name = alg_names[j]
+    for i in range(len(alg_names_r)):
+        for j in range(len(alg_names_y)):
+            alg1_name = alg_names_r[i]
+            alg2_name = alg_names_y[j]
             wins1, wins2 = 0, 0
-
-            print("Current players: ", alg1_name, " (Red) vs ", alg2_name, " (Yellow)")
-            for game_num in range(100): #Play 100 games
-                print("Playing game: ", game)
+            
+            print("current players: ", alg1_name," and ", alg2_name)
+            for game_num in range(100):
+                print("playing game: ", game)
                 game += 1
-
-                #Alternate first player
-                if game_num % 2 == 0:
-                    red_alg, yellow_alg = alg1_name, alg2_name
-                else:
-                    red_alg, yellow_alg = alg2_name, alg1_name
-
-                #Initialize the board
+                
                 board = Board()
-                board.bindPlayer(alg_configs[red_alg](), "R")
-                board.bindPlayer(alg_configs[yellow_alg](), "Y")
-
-                #Play the game
+                board.bindPlayer(alg_configs[alg1_name](), "R")
+                board.bindPlayer(alg_configs[alg2_name](), "Y")
+                
                 result = None
                 while result is None:
-                    result = board.turn(verbosity="None", parameter=10000)
+                    result = board.turn(verbosity="None", parameter=0)
 
-                #Determine the winner
-                if result == 1: #Yellow wins
-                    winner = yellow_alg if board.currentTurn == "R" else red_alg
-                elif result == -1: #Red wins
-                    winner = red_alg if board.currentTurn == "R" else yellow_alg
+                if result == 1:  # Yellow wins
+                    wins2 += 1 
+                elif result == -1:  # Red wins
+                    wins1 += 1 
                 else:
-                    winner = None #Draw
+                    winner = None  # draw
 
-                #Update win counts
-                if winner == alg1_name:
-                    wins1 += 1
-                elif winner == alg2_name:
-                    wins2 += 1
+            results[alg1_name][alg2_name] = (wins1 / 100) * 100  # percentage
 
-            #Store results as percentages
-            results[alg1_name][alg2_name] = (wins1 / 100) * 100  # Percentage of wins
-
-    #Print results as table
+    # Print results as table
     print("\nTournament Results (% win rate):")
-    print(f"{'':>20}", end='')
+    print(f"{'':>15}", end='')
     for name in alg_names:
-        print(f"{name:>20}", end='')
+        print(f"{name:>15}", end='')
     print()
     for name1 in alg_names:
-        print(f"{name1:>20}", end='')
+        print(f"{name1:>15}", end='')
         for name2 in alg_names:
             win_pct = results[name1][name2]
-            print(f"{win_pct:20.2f}", end='')
+            print(f"{win_pct:15.2f}", end='')
         print()
 
-
-# run_tournament()
-test()
+alg_list = ['UCT-10000' , 'Improved-UCT-10000']
+red_list = [1]
+yellow_list = [0]
+run_tournament([alg_list[i] for i in red_list], [alg_list[j] for j in yellow_list])
+# test() To run specific amount of simmulations per alg
